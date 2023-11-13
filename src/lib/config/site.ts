ec25dd8b0effe3d7d8429cc1e8111be467a87a82
dakkenkd <dakkenkd425@gmail.com>
2023-11-13 14:51:35 +0900

    update

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'tyama-blog',
+  subtitle: '開発や研究に関するブログ',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    avatar: '/assets/kuroneko.jpeg',
+    name: 'Tyama',
+    status: '😁',
+    bio: 'student(M1)'
   },
   themeColor: '#3D4451'
 }
